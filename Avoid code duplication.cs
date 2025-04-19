e4c7dc21b0425944885ef0c2b6b14379472ff875
Ursego <90226705+Ursego@users.noreply.github.com>
2025-04-19 17:14:56 -0400

    Add files via upload

--- Avoid code duplication.cs
+++ Avoid code duplication.cs
@@ -122,6 +122,122 @@ alert(changeCase("hello", "upper"));
 alert(changeCase("WORLD", "lower"));
 
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// If you often query data from a single database table using different search criteria, create one generic stored procedure.  
+// Pass the optional search criteria through optional (i.e. with default values) parameters.
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+// Then, call this procedure from various places, passing custom parameter sets as needed.
+// Obviously, the WHERE clause should restrict only by the parameters through which the values ​​are actually passed.
+
+// Now I'll show you a real Sybase T-SQL procedure I wrote while working at an insurance company.
+// Requests for the number of client claims are very common in the system—for example, to determine eligibility for certain coverages or to calculate premiums.  
+// While the eligibility criteria and calculation rules vary widely, the core logic stays the same, and the search is always performed across two tables.
+// The total length of duplicated code through the system was shocking! I couldn’t stand it any longer, and wrote this procedure:
+
+CREATE PROCEDURE s_get_clm_cnt
+   @pol_no             numeric(12),
+   @pol_ver_dt         smalldatetime,
+   @prior_to_dt        smalldatetime,   -- usually pass pol_term_eff_dt, but not always - depending on business requirements
+   @period_in_years    smallint,        -- for how much time before @prior_to_dt
+   @at_fault           char(1),         -- 'Y' = AF only, 'N' = not-AF only, 'A' = Any (AF or not-AF)
+   @before_period      char(1) = 'N',   -- 'Y' = BEFORE @period_in_years, 'N' = DURING @period_in_years; usually 'N', so you can omit this arg in most cases
+   @pol_ver_planloc_no smallint = NULL, -- vehicle; omit to count all vehicles on policy
+   @parm__pol_drv_no   smallint = NULL, -- omit if you don't need to restrict by it (i.e. if you need to count all drivers on vehicle)
+   @parm__pol_clm_typ  tinyint  = NULL, -- omit if you don't need to restrict by it.
+                                        -- The arg is ignored if @at_fault = 'Y'. In thes case, the filter is: pol_clm_typ = 100 /* Chargeable */
+   @parm__plan_cd      smallint = NULL, -- omit if you don't need to restrict by it
+   @clm_cnt            smallint OUTPUT
+AS
+/******************************************************************************************************************************************
+Returns the total count of claims for given vehicle for given period of time per given parameters.
+*******************************************************************************************************************************************
+Michael Zuskin 29-Oct-2019  Created for Portal UW Rules
+*******************************************************************************************************************************************/
+
+DECLARE
+   @nm_no           int,
+   @gis_clm_cnt     smallint,
+   @non_gis_clm_cnt smallint
+
+IF @parm__pol_drv_no IS NOT NULL BEGIN
+   SELECT @nm_no = nm_no
+     FROM pol_drv
+    WHERE pol_no     = @pol_no
+      AND pol_ver_dt = @pol_ver_dt
+      AND pol_drv_no = @parm__pol_drv_no
+END
+
+SELECT @gis_clm_cnt = count(*)
+  FROM pol_ver_clm c
+      ,pol_ver v
+ WHERE c.pol_no             = @pol_no
+   AND c.pol_ver_dt         = @pol_ver_dt
+   AND (c.charge_planloc_no = @pol_ver_planloc_no OR @pol_ver_planloc_no IS NULL)
+   AND v.pol_no             = c.pol_no
+   AND v.pol_ver_dt         = c.pol_ver_dt
+   
+   AND sa_clm_appl_cd      IN (1 /* Overriden */, 4 /* Automatic */)
+   AND sa_clm_no           IS NULL -- Ignore Related Claims
+
+   AND (c.drv_nm_no   = @nm_no             OR @parm__pol_drv_no  IS NULL) 
+   AND (c.pol_clm_typ = @parm__pol_clm_typ OR @parm__pol_clm_typ IS NULL)
+   AND (c.plan_cd     = @parm__plan_cd     OR @parm__plan_cd     IS NULL)
+
+   AND (
+          (@before_period = 'Y' AND c.loss_dt <  DateAdd (year, (@period_in_years * -1), @prior_to_dt))
+          OR
+          (@before_period = 'N' AND c.loss_dt >= DateAdd (year, (@period_in_years * -1), @prior_to_dt))
+       )
+
+   AND (
+          (@at_fault IN ('Y', 'A') AND c.pol_clm_typ =  100 /* Chargeable */)
+          OR
+          (@at_fault IN ('N', 'A') AND c.pol_clm_typ <> 100 /* Chargeable */)
+       )
+
+SELECT @non_gis_clm_cnt = count(*)
+  FROM pol_ver_oth_clm c, 
+       nm_drv_oth_clm_ver v,
+       pol_ver pv
+ WHERE c.pol_no                = @pol_no
+   AND c.pol_ver_dt            = @pol_ver_dt
+   AND (c.charge_planloc_no    = @pol_ver_planloc_no OR @pol_ver_planloc_no IS NULL)
+   AND pv.pol_no               = c.pol_no
+   AND pv.pol_ver_dt           = c.pol_ver_dt
+   AND c.nm_no                 = v.nm_no
+   AND c.nm_drv_oth_clm_no     = v.nm_drv_oth_clm_no
+   AND c.nm_drv_oth_clm_ver_dt = v.nm_drv_oth_clm_ver_dt
+
+   AND sa_clm_appl_cd          IN (1 /* Overriden */, 4 /* Automatic */)
+   AND v.oth_clm_amt           >= 0                              
+   AND isnull(c.chg_sta_cd,'N') <> 'D'
+
+   AND ((v.nm_no = @nm_no OR v.drv_nm_no = @nm_no) OR @parm__pol_drv_no  IS NULL)
+   AND (v.pol_clm_typ = @parm__pol_clm_typ         OR @parm__pol_clm_typ IS NULL)
+   AND (v.plan_cd     = @parm__plan_cd             OR @parm__plan_cd     IS NULL)
+
+   AND (
+          (@before_period = 'Y' AND v.loss_dt <  DateAdd (year, (@period_in_years * -1), @prior_to_dt))
+          OR
+          (@before_period = 'N' AND v.loss_dt >= DateAdd (year, (@period_in_years * -1), @prior_to_dt))
+       )
+
+   AND (
+          (@at_fault IN ('Y', 'A') AND v.pol_clm_typ =  100 /* Chargeable */)
+          OR
+          (@at_fault IN ('N', 'A') AND v.pol_clm_typ <> 100 /* Chargeable */)
+       )
+
+SET @clm_cnt = IsNull(@gis_clm_cnt, 0) + IsNull(@non_gis_clm_cnt, 0)
+
+RETURN
+
+// The reaction of some colleagues who had been on the project for years was interesting.  
+// Some looked at me like a super-programmer — even though writing this procedure (and a few others like it) was far easier than launching a rocket to Mars.  
+// And one developer said these procedures shouldn’t be used—for the sake of consistency with the old code.
+// But I have no interest in being consistent with bad practices!
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Use CHOOSE CASE to avoid duplication od SQL queries
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
